3d7b4bf7ab17b340d5aedecea475eb3f17620971
VitorArimatea <vitorarimatea@hotmail.com>
2025-01-23 11:48:46 -0300

    new: Initial setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-around p-10">
+      <h1 className="text-2xl">Hello, I'm Vítor Arimatéa</h1>
+      <p className="text-justify mt-10">Estou super ansioso para dar vida a essa aplicação tão íncrivel inspirada no Notion! Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita sunt beatae dignissimos cum, illo enim ipsum. Quos placeat tempora sint consectetur. Voluptatum consequuntur similique at, ipsa dolorum suscipit. Inventore, maiores.</p>
+    </main>
+  );
+}
