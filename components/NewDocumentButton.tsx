c0075177ccc37e33e17b958a5f5bf32dcfa76d9d
VitorArimatea <vitorarimatea@hotmail.com>
2025-04-04 17:30:50 -0300

    feat: NewDocumentButton component created

---
+++ components/NewDocumentButton.tsx
@@ -0,0 +1,10 @@
+import { Button } from "./ui/button";
+
+function NewDocumentButton() {
+  return (
+    <div>
+      <Button className="cursor-pointer">Novo Documento</Button>
+    </div>
+  );
+}
+export default NewDocumentButton;
